12e8d32dda306034c832f605561c5d5e424f5d3d
preethijc <preethi.jc92@hotmail.com>
2019-03-19 11:48:16 -0700

    Added Winning Condition

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ class Minesweeper(object):
         self.createBoard()
 
         print "Placing Mines..."
-        self.mines = self.placeMines()
-        # Example mines set to test:
-        # set([(2, 6), (7, 0), (3, 3), (5, 6), (4, 5), (1, 6), (4, 3), (0, 3), (6, 5), (5, 3)])
+        self.placeMines()
+
+        # Example mines set to test (Comment Line 12 and uncomment Line 15):
+        # self.mines = set([(2, 6), (7, 0), (3, 3), (5, 6), (4, 5), (1, 6), (4, 3), (0, 3), (6, 5), (5, 3)])
 
         print "To quit, enter q\n"
         self.printBoard()
 
+        self.moves = set()
         move = raw_input("\nYour Move: ")
+
         while True:
             if move.lower() == "q":
                 break
@@ -27,9 +30,23 @@ class Minesweeper(object):
                 print "Mine Detected. Game Over!"
                 break
 
+            self.moves.add(square)
+
             self.board[square[0]][square[1]] = self.countMines(square)
             self.printBoard()
+
+            if self.isOver():
+                print "\nYou Win!! Game Over!"
+                break
+
             move = raw_input("\nYour Move: ")
+
+    def isOver(self):
+        remSquares = set(self.squares)-self.moves
+        mineSquares = remSquares - self.mines
+        if not mineSquares:
+            return True
+        return False
 
 
     def countMines(self, square):
@@ -59,7 +76,7 @@ class Minesweeper(object):
         for i in range(0, 8):
             row = []
             for j in range(0, 8):
-                self.squares.append([i, j])
+                self.squares.append((i, j))
                 row.append(" ")
             self.board.append(row)
 
